609c9ab8c66b74eb563b25179ffe846b90e16387
Gabriel Barboza <65047738+gabrieIbarboza@users.noreply.github.com>
2023-03-25 14:28:53 -0300

    First class

--- aula01_fatorial.c
+++ aula01_fatorial.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+//
+// Aula 01 - Introdução aos conceitos de Algoritmos e Lógica de Programação
+//
+
+//Function para calcular fatorial de um número
 int CalcFat(int n) {
     if(n == 0 || n == 1)
         return 1;
     
+    //Chama ela mesma até que n seja 1 ou 0
     return n * (CalcFat(n-1));
 }
 
+//Todo programa em C sempre terá uma function main()
 int main()
 {
     int nfat = -1;
